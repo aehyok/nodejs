0fe5d80fe23fd9427c7b8b73d405c12735061d06
刘启明 <aehyok@vip.qq.com>
2017-08-31 18:35:39 +0800

    nodejs安装express,通过express测试两个Rest Api

---
+++ expressApp.js
@@ -0,0 +1,20 @@
+//expressApp.js 文件
+var express = require('express');
+var app = express();
+ 
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+ 
+app.get('/User/', function (req, res) {
+    res.send('Hello World User');
+ })
+
+var server = app.listen(8081, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+})
