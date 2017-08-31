fa9c972d640284e59dd7297c08125e5de3110fdc
刘启明 <aehyok@vip.qq.com>
2017-08-31 18:53:19 +0800

    简单测试代码Express  route

--- expressApp.js
+++ expressApp.js
@@ -10,6 +10,31 @@ app.get('/User/', function (req, res) {
     res.send('Hello World User');
  })
 
+//  POST 请求
+app.post('/', function (req, res) {
+    console.log("主页 POST 请求");
+    res.send('Hello POST');
+ })
+  
+ //  /del_user 页面响应
+ app.get('/del_user', function (req, res) {
+    console.log("/del_user 响应 DELETE 请求");
+    res.send('删除页面');
+ })
+  
+ //  /list_user 页面 GET 请求
+ app.get('/list_user', function (req, res) {
+    console.log("/list_user GET 请求");
+    res.send('用户列表页面');
+ })
+  
+ // 对页面 abcd, abxcd, ab123cd, 等响应 GET 请求
+ app.get('/ab*cd', function(req, res) {   
+    console.log("/ab*cd GET 请求");
+    res.send('正则匹配');
+ })
+  
+
 var server = app.listen(8081, function () {
  
   var host = server.address().address
