ee5c110161141bf55ee7609c0cbb4f1f5229f9ab
刘启明 <aehyok@vip.qq.com>
2017-09-01 17:59:36 +0800

    nodejs连接mongodb简单增删改查

--- mongodbApp.js
+++ mongodbApp.js
@@ -16,9 +16,57 @@ var insertData = function(db, callback) {
     });
 }
  
+var selectData = function(db, callback) {  
+    //连接到表  
+    var collection = db.collection('Customer');
+    //查询数据
+    var whereStr = {"name":'菜鸟教程'};
+    collection.find(whereStr).toArray(function(err, result) {
+      if(err)
+      {
+        console.log('Error:'+ err);
+        return;
+      }     
+      callback(result);
+    });
+  }
+   
+
+var updateData = function(db, callback) {  
+    //连接到表  
+    var collection = db.collection('Customer');
+    //更新数据
+    var whereStr = {"name":'菜鸟教程'};
+    var updateStr = {$set: { "url" : "https://www.runoob.com" }};
+    collection.update(whereStr,updateStr, function(err, result) {
+        if(err)
+        {
+            console.log('Error:'+ err);
+            return;
+        }     
+        callback(result);
+    });
+}
+
+var delData = function(db, callback) {  
+    //连接到表  
+    var collection = db.collection('Customer');
+    //删除数据
+    var whereStr = {"name":'菜鸟工具'};
+    collection.remove(whereStr, function(err, result) {
+      if(err)
+      {
+        console.log('Error:'+ err);
+        return;
+      }     
+      callback(result);
+    });
+  }
+
+
 MongoClient.connect(DB_CONN_STR, function(err, db) {
     console.log("连接成功！");
-    insertData(db, function(result) {
+    selectData(db, function(result) {
         console.log(result);
         db.close();
     });
