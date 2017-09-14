c19420993848b497b36dbf4a6c4374d10e26c24b
刘启明 <aehyok@vip.qq.com>
2017-09-14 15:35:36 +0800

    express Mongoose处理参考Github上的开源项目

--- schemas/index.js
+++ schemas/index.js
@@ -1,7 +1,7 @@
 
 var mongoose = require('mongoose');
 var db_url="mongodb://192.168.1.100:27017/aehyok";
-
+//https://github.com/54sword/api.xiaoduyu.com  express Mongoose处理参考Github上的开源项目
 
 mongoose.connect(db_url, {useMongoClient: true},function (error) {
 	if (error) {
