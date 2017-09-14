d7c8deea4de752280101545332e8ced13b21ed2b
刘启明 <aehyok@vip.qq.com>
2017-09-14 15:33:07 +0800

    mongoose基本操作以及API

--- models/toBid.js
+++ models/toBid.js
@@ -1,5 +1,7 @@
 var toBid = require('../schemas').toBid;
 
+//Mongoose 基本操作和API列表  https://cnodejs.org/topic/51ff720b44e76d216afe34d9
+
 // 插入数据
 exports.save = function(data, callback) {
   new toBid(data).save(callback);
