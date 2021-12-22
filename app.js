d1185e821fb5cc39ec06911fe6f92827cf04b97f
刘启明 <455043818@qq.com>
2021-12-22 11:24:10 +0800

    chore: 重新整理文件

--- app.js
+++ app.js
@@ -1,102 +1,20 @@
-'use strict'
-//https://github.com/ivonzhang/koa2-mongodb-server
-const fs = require('fs')
-const path = require('path')
-const mongoose = require('mongoose')
+const Koa = require('koa');
 
-const db_url="mongodb://192.168.1.100:27017/aehyok";
+const router = require('./router')
 
-/**
- * mongoose连接数据库
- * @type {[type]}
- */
-mongoose.Promise = require('bluebird')
-//mongoose connection打开
-mongoose.connect(db_url, {useMongoClient: true});
+const app = new Koa();
 
-//如果连接成功会执行error回调
-mongoose.connection.on("error", function (error) {
-    console.log("数据库连接失败：" + error);
+const port = '8787'
+const host = '0.0.0.0'
+
+app.use(router.routes());
+/*
+    原先当路由存在，请求方式不匹配的时候，会报 404，
+    加了这个中间件，会报请求方式不被允许
+*/
+app.use(router.allowedMethods());
+
+
+app.listen(port, host, () => {
+    console.log(`API server listening on ${host}:${port}`);
 });
-
-//如果连接成功会执行open回调
-mongoose.connection.on("open", function () {
-    console.log("数据库连接成功");
-});
-
-
-
-/**
- * 获取数据库表对应的js对象所在的路径
- * @type {[type]}
- */
-const models_path = path.join(__dirname, '/koa/models')
-
-
-/**
- * 已递归的形式，读取models文件夹下的js模型文件，并require
- * @param  {[type]} modelPath [description]
- * @return {[type]}           [description]
- */
-var walk = function(modelPath) {
-  fs
-    .readdirSync(modelPath)
-    .forEach(function(file) {
-      var filePath = path.join(modelPath, '/' + file)
-      //var stat = fs.stat(filePath);
-      var stat = fs.statSync(filePath);
-
-      if (stat.isFile()) {
-        if (/(.*)\.(js|coffee)/.test(file)) {
-          require(filePath)
-        }
-      }
-      else if (stat.isDirectory()) {
-        walk(filePath)
-      }
-    })
-}
-walk(models_path)
-
-require('babel-register')
-const Koa = require('koa')
-//const logger = require('koa-logger')
-////const session = require('koa-session')
-//const bodyParser = require('koa-bodyparser')
-const app = new Koa()
-
-app.keys = ['zhangivon']
-//app.use(logger())
-//app.use(session(app))
-//app.use(bodyParser())
-
-
-/**
- * 使用路由转发请求
- * @type {[type]}
- */
-const router = require('./config/router')()
-
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  console.log(`first ${ctx.method} ${ctx.url} - ${ms}ms`);
-});
-
-app.use((ctx, next) => {
-  const start = Date.now();
-  return next().then(() => {
-    const ms = Date.now() - start;
-    console.log(`second ${ctx.method} ${ctx.url} - ${ms}ms`);
-  });
-});
-
-app
-  .use(router.routes())
-  .use(router.allowedMethods());
-
-
-
-app.listen(3000)
-console.log('app started at port 3000...');
